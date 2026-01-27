5ad7f19051ee3244663a5bb7b075434123fc86e8
makzomltd <makzomltd@gmail.com>
2026-01-27 13:35:19 +0200

    no thing

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,9 @@
 import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
 
+import en from  "./locales/en.json"
+import ar from "./locales/ar.json"
+
 
 i18n
 .use(initReactI18next)
